372c9cfe7281a02c700a16f49b06570cb6f6a580
songliangcheng <1062670898@qq.com>
2021-12-16 16:53:00 +0800

    init

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// https://hexo.io/api/filter#before-post-render
-hexo.extend.filter.register('before_post_render', require('./lib').default);
+'use strict';
 
-hexo.extend.filter.unregister('before_post_render', require('./lib').default);
+// https://hexo.io/api/filter#before-post-render
+hexo.extend.filter.register('before_post_render', require('./lib'), 10);
