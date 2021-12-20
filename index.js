3ddb4d46605150b7f69d9cf80f52a5b189387282
slcnx <35824750+slcnx@users.noreply.github.com>
2021-12-20 23:11:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict'
-const Hexo = require('hexo')
-const hexo = new Hexo(process.cwd(), {})
+'use strict';
+const Hexo = require('hexo');
+const hexo = new Hexo(process.cwd(), {});
 
 // https://hexo.io/api/filter#before-post-render
-hexo.extend.filter.register('before_post_render', require('./lib'), 10)
+hexo.extend.filter.register('before_post_render', require('./lib'), 10);
