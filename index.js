ec097044cb5da2fb66e400c6d1851029d035f606
slcnx <35824750+slcnx@users.noreply.github.com>
2021-12-21 20:17:23 +0800

    update after_post_render

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict'
 
 // https://hexo.io/api/filter#before-post-render
-hexo.extend.filter.register('before_post_render', require('./lib'), 10)
+hexo.extend.filter.register('after_post_render', require('./lib'), 10)
