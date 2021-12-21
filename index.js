6ae134d7c3f3f8c6bf7ec34f11d936f1689f7a9b
songliangcheng <1062670898@qq.com>
2021-12-21 12:34:50 +0800

    hexo 插件
    代码质量中的hexo未定义可以忽略，故去掉质量检测

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-'use strict';
-const Hexo = require('hexo');
-const hexo = new Hexo(process.cwd(), {});
+'use strict'
 
 // https://hexo.io/api/filter#before-post-render
-hexo.extend.filter.register('before_post_render', require('./lib'), 10);
+hexo.extend.filter.register('before_post_render', require('./lib'), 10)
